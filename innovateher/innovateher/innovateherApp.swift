9b1a76e5ba0698015418b3b67e3803f9f0a7c77e
Hannah Cha <hannahcha@Hannahs-MacBook-Pro.local>
2025-01-24 20:20:20 -0500

    initial commit

---
+++ innovateher/innovateher/innovateherApp.swift
@@ -0,0 +1,17 @@
+//
+//  innovateherApp.swift
+//  innovateher
+//
+//  Created by Hannah Cha on 1/24/25.
+//
+
+import SwiftUI
+
+@main
+struct innovateherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
